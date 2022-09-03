a1921992448cb3f43bad8626d38b5dc62754015c
Yulia <rosiely@mail.ru>
2022-09-03 09:31:45 +0500

    все задачи с горем пополам

---
+++ Task_58/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+//Например, даны 2 матрицы:
+//2 4 | 3 4
+//3 2 | 3 3
+//Результирующая матрица будет:
+//18 20
+//15 18
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 6); //Для увеличения размера чисел в матрицах поменять число 6 на большее
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+Console.Clear();
+
+int sizeAi = InputInt("введите количество строк матрицы А: ");
+int sizeAj = InputInt("введите количество столбцов матрицы А: ");
+int[,] matrixA = new int[sizeAi, sizeAj];
+Console.WriteLine($"По правилу перемножения матриц количество столбцов матрицы А должно быть равно количеству строк матрицы В и равна {sizeAj}");
+int sizeBj = InputInt("введите количество столбцов матрицы B: ");
+int[,] matrixB = new int[sizeAj, sizeBj];
+FillArrayRandomNumbers(matrixA);
+FillArrayRandomNumbers(matrixB);
+int[,] matrixC = new int[sizeAi, sizeBj];
+
+for (int i = 0; i < sizeAi; i++)
+{
+    for (int j = 0; j < sizeBj; j++)
+    {
+        for (int k = 0; k < sizeAj; k++)
+        {
+            matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
+        }
+    }
+}
+Console.WriteLine("Матрица - А");
+PrintArray(matrixA);
+Console.WriteLine();
+Console.WriteLine("Матрица - В");
+PrintArray(matrixB);
+Console.WriteLine();
+Console.WriteLine("Произведение матриц А*В");
+PrintArray(matrixC);
+
---
+++ Task_62/Program.cs
@@ -0,0 +1,78 @@
+﻿// Напишите программу, которая заполнит спирально прямоугольного массива.
+//Например, на выходе получается вот такой массив:
+//01 02 03 04
+//12 13 14 05
+//11 16 15 06
+//10 09 08 07
+
+void PrintArray(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write("[ ");
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            if (array[i, j] > 9)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            else Console.Write("0" + array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+Console.Clear();
+
+Console.WriteLine($"\nВведите размер массива m x n: ");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int[,] nums = new int[m, n];
+int contnum = m * n;
+int num = 2, minI = 0, minJ = 1;
+int i = 0, j = 0 ;
+nums[0,0] = 1;
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+while (num <= contnum)
+{
+    while (i + 1 < m && nums[i + 1, j] == 0)
+        { 
+        i++;
+        nums[i, j] = num;
+        num++;
+        }
+    while (j + 1 < n && nums[i, j + 1] == 0)
+        {
+        j++;
+        nums[i, j] = num;
+        num++;     
+        }    
+    while ( i - 1 > minI - 1 && nums[i -1 ,j] == 0)
+        {
+        i--;
+        nums[i, j] = num;
+        num++;   
+        }
+    while (j - 1> minJ - 1 &&  nums[i, j - 1] == 0)
+        {
+        j--;
+        nums[i, j] = num;
+        num++;
+        } 
+}
+
+ PrintArray(nums);
+
+
+
+
+
